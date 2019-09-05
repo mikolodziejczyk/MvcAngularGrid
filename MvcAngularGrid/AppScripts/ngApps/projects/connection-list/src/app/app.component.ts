5c8d642abfbb8f645bc1af065ea707373d1c9aef
mko <gogs@fake.local>
2019-09-05 13:36:32 +0200

    Added correct links

--- MvcAngularGrid/AppScripts/ngApps/projects/connection-list/src/app/app.component.ts
+++ MvcAngularGrid/AppScripts/ngApps/projects/connection-list/src/app/app.component.ts
@@ -21,7 +21,10 @@ export class AppComponent implements OnInit, OnDestroy {
   constructor (private http: HttpClient) {
   }
 
-  url: string = '/data/ep/page';
+  dataUrl: string = '/data/ep/page';
+  newUrl: string = '/ep/new';
+  displayUrl: string = '/ep/details/';
+
   globalFilterControl: FormControl = new FormControl();
   globalFilterControlSubscription: Subscription;
   globalFilterControlDebouncer: Debouncer = new Debouncer();
@@ -38,14 +41,14 @@ export class AppComponent implements OnInit, OnDestroy {
   columnDefs = [
     {
       headerName: 'Ppe', field: 'ppe',
-      cellRenderer: linkRenderer,
+      cellRenderer: (params: ICellRendererParams) => this.linkRenderer(params),
       sortable: true,
       filter: true,
       filterParams: { suppressAndOrCondition: true }
     },
     {
       headerName: 'MeterCode', field: 'meterCode',
-      cellRenderer: linkRenderer,
+      cellRenderer: (params: ICellRendererParams) => this.linkRenderer(params),
       sortable: true,
       filter: true,
       filterParams: { suppressAndOrCondition: true }
@@ -116,7 +119,7 @@ export class AppComponent implements OnInit, OnDestroy {
       const requestBody: IAgGridDataRequest = PrepareAgGridDataRequest(params, this.globalFilter);
 
       try {
-        data = await this.http.post<IDataResponse>(this.url, requestBody).toPromise();
+        data = await this.http.post<IDataResponse>(this.dataUrl, requestBody).toPromise();
       } catch (e) {
         params.failCallback();
         return;
@@ -147,8 +150,9 @@ export class AppComponent implements OnInit, OnDestroy {
 
   onRowDoubleClicked = (event: RowDoubleClickedEvent) => {
     if (event.data) {
-      // tslint:disable-next-line:no-string-literal
-      alert(`Navigate to: ${event.data['id']}.`);
+      const id = event.data.id;
+      const url = this.displayUrl + id.toString();
+      window.location.href = url;
     }
   }
 
@@ -156,6 +160,24 @@ export class AppComponent implements OnInit, OnDestroy {
     this.globalFilter = filterText;
     this.gridOptions.api.onFilterChanged();
   }
+
+  linkRenderer(params: ICellRendererParams): string {
+    const text = params.value;
+    let r = null;
+
+    if (params.data) {
+      const id = params.data.id;
+      const url = this.displayUrl + id.toString();
+
+      if (text) {
+        r = `<a href="${url}">${text}</a>`;
+      }
+    }
+
+    return r;
+  }
+
+
 }
 
 function gridDateFormatter(params: ValueFormatterParams): any {
@@ -164,22 +186,5 @@ function gridDateFormatter(params: ValueFormatterParams): any {
   if (date) {
     r = formatDate(date, 'yyyy-MM-dd', 'en-US');
   }
-  return r;
-}
-
-function linkRenderer(params: ICellRendererParams): string {
-  const text = params.value;
-  let r = null;
-
-  if (params.data) {
-    // tslint:disable-next-line:no-string-literal
-    const id = params.data['id'];
-    const url = `/connection/details/` + id;
-
-    if (text) {
-      r = `<a href="${url}">${text}</a>`;
-    }
-  }
-
   return r;
 }
